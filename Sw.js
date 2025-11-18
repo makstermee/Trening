14ad9f1f0334880e275121d152319c14ab26883d
makstermee <michalnowicki000@gmail.com>
2025-11-18 07:05:44 +0100

    Add files via upload

---
+++ Sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'gympro-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;800&display=swap'
+];
+
+// Instalacja i cache'owanie plików
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Pobieranie danych (korzystanie z cache gdy brak internetu)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
